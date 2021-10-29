7a0a2b2ba8b67bf3bb75ae24ffc26f839bbdc6ef
Jonathan Francisco <thinklikejo19@gmail.com>
2021-10-29 22:47:33 +0800

    Add deliveries tab and processed orders tab content

--- lib/screens/orders/widgets/deliveries-tab.dart
+++ lib/screens/orders/widgets/deliveries-tab.dart
@@ -1,8 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:sandys_food_express/constants.dart';
 
 class DeliveriesTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Hello World');
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                showBottomBorder: true,
+                dividerThickness: 0.25,
+                headingTextStyle: TextStyle(
+                  color: tableColTextColor,
+                ),
+                columns: [
+                  DataColumn(
+                    label: Text(
+                      'Code',
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Name',
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Total',
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Delivery Time',
+                    ),
+                  ),
+                ],
+                rows: List.generate(
+                  20,
+                  (index) {
+                    return DataRow(
+                      color: MaterialStateProperty.all(Colors.white),
+                      cells: [
+                        DataCell(
+                          Text(
+                            'REF12345',
+                            style: TextStyle(
+                              color: tableRowTextColor,
+                            ),
+                          ),
+                        ),
+                        DataCell(Text('Jonathan',
+                            style: TextStyle(
+                              color: tableRowTextColor,
+                            ))),
+                        DataCell(
+                          Text(
+                            '100.00',
+                            style: TextStyle(
+                              color: primaryColor,
+                            ),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            '8:00 AM',
+                            style: TextStyle(
+                              color: tableRowTextColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
